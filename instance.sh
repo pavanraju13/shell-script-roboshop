99ca3f212068afe8a49b5f83f29174e2557cde5f
pavanraju13 <pavanraju4513@gmail.com>
2025-06-17 21:27:04 +0530

    servers configuration

---
+++ instance.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0529dc4ac677bb0be"
+Instances=$1
+Time_stamp=$( date + %Y-%m-%d-%H-%M-%S )
+
+for instance in $Instances
+do 
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.small --security-group-ids sg-0529dc4ac677bb0be --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+if [ $instance != "frontend" ]
+then
+IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+else
+IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+        fi
+    echo "$instance IP address: $IP"
