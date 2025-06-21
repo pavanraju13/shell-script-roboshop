063b26447c8753d550f1611d7f32ae23365c0fe5
pavanraju13 <pavanraju4513@gmail.com>
2025-06-21 08:11:47 +0530

    servers configuration

--- instance.sh
+++ instance.sh
@@ -2,11 +2,11 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0529dc4ac677bb0be"
-Instances=$1
+
 Time_stamp=$( date +%Y-%m-%d-%H-%M-%S )
 DOMAIN_NAME=clouddevops.life
 
-for instance in $Instances
+for instance in $@
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.small --security-group-ids sg-0529dc4ac677bb0be --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 if [ $instance != "frontend" ]
