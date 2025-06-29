5f63dfbb0224e8ee82adfeb43852d84ea6e4ce94
pavanraju13 <pavanraju4513@gmail.com>
2025-06-29 19:00:26 +0530

    servers configuration

--- route53.sh
+++ route53.sh
@@ -5,28 +5,26 @@ IP=$2
 instance=$3
 ZONE_ID=Z0511103ULD2JWV1IRW1
 
-
-
 aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change-batch '
+    --change-batch "
     {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
+        \"Comment\": \"Creating or Updating a record set for $instance\",
+        \"Changes\": [{
+            \"Action\": \"UPSERT\",
+            \"ResourceRecordSet\": {
+                \"Name\": \"$RECORD_NAME\",
+                \"Type\": \"A\",
+                \"TTL\": 1,
+                \"ResourceRecords\": [{
+                    \"Value\": \"$IP\"
+                }]
+            }
         }]
-    }' &>> /dev/null
-if [ $? -eq 0 ]
-then
-   echo -e "\e[32mRoute53 record created for $instance successfully\e[0m"
+    }" &>> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mRoute53 record created for $instance successfully\e[0m"
 else
-   echo -e "\e[31m32mRoute53 record created for $instance failure\e[0m"
+    echo -e "\e[31mRoute53 record creation for $instance failed\e[0m"
 fi
