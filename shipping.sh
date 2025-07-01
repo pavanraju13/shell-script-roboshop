86ffd2a9f37f8171d89051aace71890fcb393c37
pavanraju13 <pavanraju4513@gmail.com>
2025-07-01 08:51:39 +0530

    servers configuration

--- shipping.sh
+++ shipping.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
 TIME_STAMP=$( date +%Y-%m-%d_%H-%M-%S )
 USER_ID=$( id -u )
 SCRIPT_NAME=$(basename $0 | cut -d "." -f1 )
@@ -14,111 +13,109 @@ Current_Path=/home/ec2-user/shell-script-roboshop
 Service=/etc/systemd/system/$SCRIPT_NAME.service
 hostname=mysql.clouddevops.life
 
-R="\e[31m"   #Red color
-G="\e[32m"   #Green color
-B="\e[33m"   #Blue color
-Y="\e[34m"   #Yellow color
-M="\e[35m"   #Magenita colr
-N="\e[0m"     #Normal color
+# Color codes
+R="\e[31m"
+G="\e[32m"
+B="\e[33m"
+Y="\e[34m"
+M="\e[35m"
+N="\e[0m"
 
-echo "enter MYSQL_USERNAME:"
+# MySQL credentials
+echo "Enter MYSQL_USERNAME:"
 read -s MYSQL_USERNAME
-echo "enter MYSQL_PASSWORD:"
+echo "Enter MYSQL_PASSWORD:"
 read -s MYSQL_PASSWORD
 
+echo "Script started at $TIME_STAMP" | tee -a $LOG_FILE
 
-echo "script started at $TIME_STAMP" | tee -a $LOG_FILE
-#This is a function
+# Function to handle result checks
 RESULT() {
-
-if [  $1 -eq 0 ]
-then
-echo -e "$2.. ${G}successfully ${N}" 
-else
-echo -e "$2..${R} failure ${N}"
-exit 2
-fi
+  if [ $1 -eq 0 ]; then
+    echo -e "$2.. ${G}successfully${N}"
+  else
+    echo -e "$2.. ${R}failure${N}"
+    exit 2
+  fi
 }
 
-#Checking the root access
-if [ $USER_ID -eq 0 ]
-then
-echo -e "${G} you are a root user ${N}" | tee -a $LOG_FILE
+# Root access check
+if [ $USER_ID -ne 0 ]; then
+  echo -e "${R}Error: You don't have root privileges${N}" | tee -a $LOG_FILE
+  exit 1
 else
-echo -e "${R} Error Permission denied you don't have root privileges ${N}" | tee -a $LOG_FILE
-exit 1
+  echo -e "${G}You are a root user${N}" | tee -a $LOG_FILE
 fi
 
+# Install pkg1 if not installed
 dnf list installed $pkg1 &>> $LOG_FILE
-if [ $? -ne 0 ]
-then
-echo "$pkg1 is not installed..${Y}installing $pkg1{N}"
-dnf install $pkg1 -y &>> $LOG_FILE
-RESULT $? "Installing $pkg1"
+if [ $? -ne 0 ]; then
+  echo "$pkg1 is not installed.. ${Y}installing $pkg1${N}"
+  dnf install $pkg1 -y &>> $LOG_FILE
+  RESULT $? "Installing $pkg1"
 else
-echo " $pkg1 already installed"
+  echo "$pkg1 already installed"
 fi
 
-id $USER
-if [ $? -ne 0 ]
-then
-echo "$USER not exists"
-echo "${M}Creating a $USER user${N}"
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" $USER
-RESULT $? "$USER created"
+# Create user if not exists
+id $USER &>> $LOG_FILE
+if [ $? -ne 0 ]; then
+  echo "${M}Creating user $USER${N}"
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" $USER
+  RESULT $? "$USER created"
 else
-echo "${Y}$USER already created${N}"
+  echo -e "${Y}$USER already exists${N}"
 fi
 
-if [ -e $DIR ]
-then
-echo "$DIR directory exists."
-else 
-echo "$DIR directory not exists.creating a directory"
-mkdir $DIR
-RESULT $? "Directory created"
+# Ensure /app directory exists
+if [ ! -d $DIR ]; then
+  echo "$DIR directory not exists. Creating..."
+  mkdir -p $DIR
+  RESULT $? "Directory created"
+else
+  echo "$DIR directory exists."
 fi
 
-curl -o $Temp_Folder https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip  &>> $LOG_FILE
-RESULT $? "downloaded the code into the $Temp_Folder"
+# Download and extract code
+curl -o $Temp_Folder https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>> $LOG_FILE
+RESULT $? "Downloaded the code into $Temp_Folder"
 
-cd $DIR                                  #switched to the app directory
+cd $DIR
 RESULT $? "Switched to $DIR"
 
-rm -rf /app/* &>> $LOG_FILE                #Deleting the old content
-RESULT $? "Deleted the content"
+rm -rf $DIR/* &>> $LOG_FILE
+RESULT $? "Deleted existing content"
 
-unzip $Temp_Folder &>> $LOG_FILE           #Unzipping the temp folder in app directory
-RESULT $? "unzipped the application code"
+unzip $Temp_Folder &>> $LOG_FILE
+RESULT $? "Unzipped the application code"
 
-mvn clean package                           #installing all the dependencies
-RESULT $? "mvn clean install"
+mvn clean package &>> $LOG_FILE
+RESULT $? "Maven clean package"
 
 cp $Current_Path/shipping.service $Service &>> $LOG_FILE
-RESULT $? "copied the service conf"
+RESULT $? "Copied the service configuration"
 
 systemctl daemon-reload &>> $LOG_FILE
 systemctl enable shipping &>> $LOG_FILE
 systemctl start shipping &>> $LOG_FILE
-RESULT $? "Reload,Enabled and Started"
+RESULT $? "Reloaded, Enabled, and Started shipping service"
 
-dnf list installed $pkg &>> $LOG_FILE
-if [ $? -ne 0 ]
-then
-echo "$pkg2 is not installed..${Y} installing $pkg2 {N}"
-dnf install $pkg2 -y &>> $LOG_FILE
-RESULT $? "Installing $pkg2"
+# Install pkg2 if not installed
+dnf list installed $pkg2 &>> $LOG_FILE
+if [ $? -ne 0 ]; then
+  echo "$pkg2 is not installed.. ${Y}installing $pkg2${N}"
+  dnf install $pkg2 -y &>> $LOG_FILE
+  RESULT $? "Installing $pkg2"
 else
-echo " $pkg2 already installed"
+  echo "$pkg2 already installed"
 fi
-# Set a sample table to check
-TABLE_TO_CHECK="users"  # Change this to any known table that will exist after loading
 
-# Check if the table exists
+# Check schema before loading
+TABLE_TO_CHECK="users"
 TABLE_EXISTS=$(mysql -h $hostname -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "USE roboshop; SHOW TABLES LIKE '$TABLE_TO_CHECK';" 2>/dev/null | grep "$TABLE_TO_CHECK")
 
 if [ -z "$TABLE_EXISTS" ]; then
-  echo "Schema not found. Loading now..." | tee -a $LOG_FILE
+  echo "Schema not found. Loading..." | tee -a $LOG_FILE
   mysql -h $hostname -u$MYSQL_USERNAME -p$MYSQL_PASSWORD < /app/db/schema.sql &>> $LOG_FILE
   mysql -h $hostname -u$MYSQL_USERNAME -p$MYSQL_PASSWORD < /app/db/app-user.sql &>> $LOG_FILE
   mysql -h $hostname -u$MYSQL_USERNAME -p$MYSQL_PASSWORD < /app/db/master-data.sql &>> $LOG_FILE
@@ -128,11 +125,7 @@ else
 fi
 
 systemctl restart shipping &>> $LOG_FILE
-RESULT $? 'restart shipping"
+RESULT $? "Restart shipping"
 
 END_STAMP=$( date +%Y-%m-%d_%H-%M-%S )
-echo "scrript completed at $END_STAMP"
-
-
-
-
+echo "Script completed at $END_STAMP"
