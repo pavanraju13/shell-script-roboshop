459f95f9cef358e9d18e66b3585a28c2c3bd7a75
pavanraju13 <pavanraju4513@gmail.com>
2025-07-01 06:49:08 +0530

    servers configuration

---
+++ mysql.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+TIME_STAMP=$( date +%Y-%m-%d_%H-%M-%S )
+USER_ID=$( id -u )
+SCRIPT_NAME=$(basename $0 | cut -d "." -f1 )
+LOG_FILE=/var/log/$SCRIPT_NAME.log
+
+R="\e[31m"   #Red color
+G="\e[32m"   #Green color
+B="\e[33m"   #Blue color
+Y="\e[34m"   #Yellow color
+M="\e[35m"   #Magenita colr
+N="\e[0m"     #Normal color
+
+#This is a function
+RESULT() {
+
+if [  $1 -eq 0 ]
+then
+echo -e "$2.. ${G}successfully ${N}" 
+else
+echo -e "$2..${R} failure ${N}"
+exit 2
+fi
+}
+
+#Checking the root access
+if [ $USER_ID -eq 0 ]
+then
+echo -e "${G} you are a root user ${N}" | tee -a $LOG_FILE
+else
+echo -e "${R} Error Permission denied you don't have root privileges ${N}" | tee -a $LOG_FILE
+exit 1
+fi
+
+echo "Enter the username of MYSQL:"
+read -s MYSQL_USERNAME
+echo "Enter the password of MYSQL"
+read -s MYSQL_PASSWORD
+
+
+dnf install mysql-server -y &>> $LOG_FILE
+RESULT $? "Installing mysql"
+
+systemctl enable mysqld &>> $LOG_FILE
+systemctl start mysqld  &>> $LOG_FILE
+RESULT $? "Enabled and started"
+
+mysql_secure_installation --set-$MYSQL_USERNAME-pass $MYSQL_PASSWORD &>> $LOG_FILE
+RESULT $? "Setted the username and password of mysql" 
