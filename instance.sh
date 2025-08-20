c5d551ccf56cc5c5ef63dab200503340b3952d79
pavanraju13 <pavanraju4513@gmail.com>
2025-08-20 10:57:57 +0530

    zone id changed

--- instance.sh
+++ instance.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME=clouddevops.life
 
 for instance in $@
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.small --security-group-ids sg-0529dc4ac677bb0be --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0fcc78c828f981df2 --instance-type t2.small --security-group-ids sg-0cdac58064d8fd8ae --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 if [ $instance != "frontend" ]
 then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
